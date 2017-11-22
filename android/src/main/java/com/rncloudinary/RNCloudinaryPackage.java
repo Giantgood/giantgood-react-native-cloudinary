fbf211b4bd4490b55257954527516376d6e3ef62
Doug Sheridan <dsherida@asu.edu>
2017-11-22 15:21:19 +0700

    remove unnecessary override

--- android/src/main/java/com/rncloudinary/RNCloudinaryPackage.java
+++ android/src/main/java/com/rncloudinary/RNCloudinaryPackage.java
@@ -16,7 +16,6 @@ public class RNCloudinaryPackage implements ReactPackage {
       return Arrays.<NativeModule>asList(new RNCloudinaryModule(reactContext));
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
       return Collections.emptyList();
     }
